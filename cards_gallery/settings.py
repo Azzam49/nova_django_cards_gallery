147c2ea3f43732137976a5c44ed1fa50d90a051f
Azzam49 <azzam49coder@gmail.com>
2025-09-03 19:24:12 +0800

    Serving static files

--- cards_gallery/settings.py
+++ cards_gallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# BASE_DIR : /Users/smartretaildevit4/Desktop/cards-gallery
+    # BASE_DIR + /static
+
+# DIRS - Directories (Folders)
+# This veriable tells django where we store our Static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    # /Users/smartretaildevit4/Desktop/cards-gallery/static
+]
